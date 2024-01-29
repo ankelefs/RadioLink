42e820d601aed6bf876b04687efac6f1cb8e1c32
ankelefs <anker@lefstad.net>
2024-01-29 08:23:16 +0100

    .

--- Total Simulation Test by Anker/totalSimulation.m
+++ Total Simulation Test by Anker/totalSimulation.m
@@ -2,19 +2,44 @@
 % filename: totalSimulation.m
 %
 % contents: This file follows the hands-on course and tries to create a
-% total simulated simulation protocol that can then be implemented on the
-% Adalm-Pluto SDR.
+% total simulated simulation protocol using BPSK that can then be implemented 
+% on the Adalm-Pluto SDR.
 %*************************************************************************
 
-% Fetches the needed parameters and functions
-run('environmentParameters.m');
-%run('environmentFunctions.m');
 
 
-sampleRate = 440;
+% PARAMETERS
+%*************************************************************************
+sampleRate = 44100;                 % Audio sample rate [Hz].
+adalmPlutoChipset = 'AD9364';       % The chosen chipset with LO tuning BW 70-6000 MHz and 56 MHz BW.
 
 
-% Creating a sine wave
-sineWave = dsp.SineWave(signalAmplitude,signalFrequency,"SampleRate";sampleRate);
 
-% NOT WORKING
+% MAIN
+%*************************************************************************
+% Define a test binary signal sequence and convert to bipolar format
+binarySignalArray = [1, 0, 0, 1, 1, 1, 0, 1, 0, 1];
+disp(binarySignalArray);
+bipolarSignalArray = binaryToBipolarFormat(binarySignalArray);
+disp(bipolarSignalArray);
+
+
+% Setup of Adalm-Pluto SDR
+connectionStatus = configurePlutoRadio(adalmPlutoChipset);
+if connectionStatus == 1
+    disp("Succesful connection to Adalm-Pluto SDR.");
+else
+    disp("Adalm-Pluto connection failed.")
+end
+
+
+
+
+
+
+% FUNCTIONS 
+%*************************************************************************
+% Changes the input array of 1s and 0s to 1s and -1s.
+function [y] = binaryToBipolarFormat(x)
+    y = 2.*x - 1;
+end
